da9c7a2090b12b149f92ba0b061a587d8f545b28
Kitlith <kitlith@kitl.pw>
2026-05-25 15:06:40 -0700

    Initial Commit
    Includes a functional mixing command line program.

---
+++ src/lib.rs
@@ -0,0 +1,145 @@
+use argh::FromArgValue;
+use itertools::Itertools;
+use nalgebra::{Matrix4, Point3, distance_squared};
+use std::{f32, fmt::Debug};
+
+#[derive(Clone, PartialEq, Hash, Eq)]
+pub struct Color(Point3<u8>);
+
+impl From<u32> for Color {
+    fn from(value: u32) -> Self {
+        let bytes = value.to_be_bytes();
+        Self(Point3::new(bytes[1], bytes[2], bytes[3]))
+    }
+}
+
+impl From<[u8; 3]> for Color {
+    fn from(value: [u8; 3]) -> Self {
+        Self(value.into())
+    }
+}
+
+impl FromArgValue for Color {
+    fn from_arg_value(value: &str) -> Result<Self, String> {
+        u32::from_str_radix(value, 16)
+            .map(<_>::into)
+            .map_err(|e| format!("Error parsing color: {}", e))
+    }
+}
+
+impl Debug for Color {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let color = (self.0[0] as u32) << 16 | (self.0[1] as u32) << 8 | (self.0[2] as u32) << 0;
+        write!(f, "Color(0x{:06x})", color)
+    }
+}
+
+impl Color {
+    // TODO: perceptual distance
+    pub fn distance_squared(&self, other: &Color) -> f32 {
+        distance_squared(&self.0.map(f32::from), &other.0.map(f32::from))
+    }
+
+    pub fn find_closest_mix<'a>(
+        &self,
+        colors: &'a [Color],
+        max_mix_colors: usize,
+        mut filter: impl FnMut(Color) -> bool,
+    ) -> (Color, Vec<&'a Color>)
+    {
+        (1..=max_mix_colors)
+            .flat_map(move |num| colors.iter().combinations_with_replacement(num))
+            .map(|mix| {
+                (
+                    mix.iter()
+                        .fold(MixedColor::default(), |mut mix, col| {
+                            mix.mix(col);
+                            mix
+                        })
+                        .result(),
+                    mix,
+                )
+            })
+            .filter(|(col, _)| filter(col.clone()))
+            .min_by(|(a, _), (b, _)| {
+                a.distance_squared(self)
+                    .total_cmp(&b.distance_squared(self))
+            })
+            .unwrap()
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct MixedColor {
+    total: Point3<i32>,
+    max: i32,
+    count: i32,
+}
+
+impl MixedColor {
+    pub fn mix(&mut self, color: &Color) -> &mut Self {
+        // let color = color.0.map(::from);
+        self.total += color.0.coords.map(i32::from);
+        self.max += color.0.coords.iter().fold(0, |a, b| a.max(*b)) as i32;
+        self.count += 1;
+        self
+    }
+
+    pub fn result(&self) -> Color {
+        let avg = self.total / self.count;
+        let avg_max = self.max / self.count;
+        let max_avg = avg.iter().fold(0, |a, b| a.max(*b));
+
+        // NOTE: This is where joy of painting color mixing diverges from armor color mixing.
+        // Armor color mixing uses a float instead of an int for the gain.
+        // While it is possible to mix colors such that gain does something
+        // (i.e. pure red + pure green + pure blue)
+        // with the base colors provided by the game the highest I've seen so far
+        // is around 1.75, which rounds down to 1 since everything here is using integers.
+        let gain: i32 = if max_avg == 0 { 0 } else { avg_max / max_avg };
+
+        Color((avg * gain).map(|c| c as u8))
+    }
+
+    // pub fn gain_factor(&self) -> f32 {
+    //     let avg = self.total / self.count;
+    //     let avg_max = self.max / self.count;
+
+    //     let max_avg = avg.iter().fold(0, |a, b| a.max(*b));
+    //     if max_avg == 0 {
+    //         0.
+    //     } else {
+    //         avg_max as f32 / max_avg as f32
+    //     }
+    // }
+}
+
+#[derive(Clone)]
+pub struct ColorBounds(pub [Color; 4]);
+
+impl ColorBounds {
+    /// The volume of the bounding tetrahedron, multiplied by 6
+    pub fn volume_6(&self) -> i64 {
+        let v = self.0.clone().map(|vn| vn.0.map(i64::from));
+        (v[0] - v[3])
+            .dot(&(v[1] - v[3]).cross(&(v[2] - v[3])))
+            .abs()
+    }
+
+    pub fn contains(&self, color: &Color) -> bool {
+        let tet_matrix = Matrix4::from_columns(&self.0.clone().map(|vn| vn.0.coords.map(f32::from).insert_row(3, 1.)));
+        let p = color.0.coords.map(f32::from).insert_row(3, 1.);
+
+        let check_sign = tet_matrix.determinant().signum();
+
+        for i in 0..4 {
+            let mut test_matrix = tet_matrix.clone();
+            test_matrix.set_column(i, &p);
+            if test_matrix.determinant().signum() != check_sign {
+                return false;
+            }
+        }
+
+        true
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,119 @@
+use std::{collections::HashMap};
+
+use itertools::Itertools;
+
+use joy_of_mixing::{Color, ColorBounds};
+
+/// Find a mix of colors that produces a target color
+#[derive(argh::FromArgs)]
+struct Args {
+    /// target color given in RGB or ARGB notation
+    #[argh(positional)]
+    target_color: Color,
+    /// number of colors to create before giving up
+    #[argh(option, default = "10")]
+    iterations: usize,
+    /// uses all colors instead of just the colors in the bounding tetrahedron
+    /// note: turning this on will make things a little bit slower
+    #[argh(switch)]
+    use_all_colors: bool,
+    /// maxiumum number of color parts to use for each new color
+    /// note: turning this higher will make things slower
+    #[argh(option, default = "8")]
+    max_mix_count: usize,
+}
+
+fn main() {
+    let mut color_names: HashMap<Color, &str> = [
+        (0xFF1D1D21u32, "Black"),
+        (0xFFB02E26, "Red"),
+        (0xFF5E7C16, "Green"),
+        (0xFF835432, "Brown"),
+        (0xFF3C44AA, "Blue"),
+        (0xFF8932B8, "Purple"),
+        (0xFF169C9C, "Aqua"),
+        (0xFF9D9D97, "Grey"),
+        (0xFF474F52, "Dark Grey"),
+        (0xFFF38BAA, "Pink"),
+        (0xFF80C71F, "Lime"),
+        (0xFFFED83D, "Yellow"),
+        (0xFF3AB3DA, "Light Blue"),
+        (0xFFC74EBD, "Magenta"),
+        (0xFFF9801D, "Orange"),
+        (0xFFF9FFFE, "White"),
+    ]
+    .into_iter()
+    .map(|(c, name)| (Color::from(c), name))
+    .collect();
+
+    let args: Args = argh::from_env();
+
+    let target_color = args.target_color;
+    println!("Target Color: {:?}", target_color);
+
+    let mut color_bounds = color_names
+        .keys()
+        .cloned()
+        .array_combinations::<4>()
+        .map(ColorBounds)
+        .filter(|b| b.contains(&target_color))
+        // TODO: find another heuristic?
+        //  maybe (signed distance from tetrahrdron, distance from the center of the tetrahedron)?
+        .min_by_key(|b| b.volume_6())
+        .expect("Target color was not inside gamut!");
+
+    println!(
+        "Starting Tetrahedron: {}, volume: {}",
+        color_bounds.0.iter().map(|c| color_names[c]).format(", "),
+        color_bounds.volume_6(),
+    );
+
+    for color_idx in 0..args.iterations {
+        // find the color part of the bounding tetrahedron that is furthest from the target color
+        color_bounds.0.sort_unstable_by(|a, b| {
+            a.distance_squared(&target_color)
+                .total_cmp(&b.distance_squared(&target_color))
+        });
+
+        let all_colors: Option<Vec<_>> =
+            args.use_all_colors.then(|| color_names.keys().cloned().collect());
+
+        let used_colors = all_colors
+            .as_ref()
+            .map(<Vec<_>>::as_slice)
+            .unwrap_or(color_bounds.0.as_slice());
+
+        let (new_color, new_color_mix) =
+            target_color.find_closest_mix(&used_colors, args.max_mix_count, |try_color| {
+                let mut new_bounds = color_bounds.clone();
+                new_bounds.0[3] = try_color.clone();
+                try_color == target_color || new_bounds.contains(&target_color)
+            });
+
+        if new_color == target_color {
+            println!("Final Mix({:?}): => [", new_color);
+            for col in new_color_mix {
+                println!("    {}", color_names[col]);
+            }
+            println!("]");
+            return;
+        }
+
+        let new_label: &str = format!("color_{}", color_idx).leak();
+
+        color_names.insert(new_color.clone(), new_label);
+
+        println!(
+            "{new_label}({new_color:?}) replacing {}: => [",
+            color_names[&color_bounds.0[3]]
+        );
+        for col in new_color_mix {
+            println!("    {}", color_names[col]);
+        }
+        println!("]");
+
+        color_bounds.0[3] = new_color;
+    }
+
+    println!("Failed to find target color in {} iterations.", args.iterations);
+}
